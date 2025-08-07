e99cdd06403df6597cce4b35b675d17569c2f013
Srujan <130295979+srujan-0@users.noreply.github.com>
2025-08-07 11:11:48 +0530

    Add files via upload

---
+++ union.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+union Student{
+	int roll_no;
+	char name[69];
+	int marks[5];
+	int total;
+};
+int main()
+ {
+	union Student s;
+	int temp_marks[5],total = 0;
+	int i;
+	
+    printf("Enter Roll Number: ");
+    scanf("%d",&s.roll_no);
+    printf("Roll Numbe: %d\n",s.roll_no);
+    
+    printf("Enter Name: ");
+    scanf("%[%s\n]",s.name);
+    printf("Name: %s\n",s.name);
+    
+    printf("Enter Marks:\n");
+    scanf("%d",&temp_marks[i]);
+    for(i=0;i<5;i++) 
+    {
+    	printf("Subject %d: ", i+1);
+    	scanf("%d",s.marks);
+    	total += temp_marks[i];
+	}
+	printf("Marks: ");
+	for(i=0; i<5; i++)
+	{
+		printf("%d",temp_marks[i]);
+	}
+    s.total=total;
+    printf("\nTotal: %d\n",s.total);
+    
+    return 0;
+}
